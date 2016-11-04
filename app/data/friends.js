8b51f1c19eb446dc3f7fc5d23ecd6eddd455de28
Nishtha <nishtha@Nishthas-MacBook-Pro.local>
2016-11-03 20:08:21 -0500

    working project

---
+++ app/data/friends.js
@@ -0,0 +1,35 @@
+
+
+module.exports.userInfoArr = [
+		{
+			name: 'Sierra',
+			link: 'https://static1.squarespace.com/static/553a8716e4b0bada3c80ca6b/553a9655e4b03939abece18a/5731fc75f85082142b12b095/1471894315703/mayfourblocknature.jpg',
+			scores: [
+				'5',
+				'3',
+				'2',
+				'1',
+				'4',
+				'3',
+				'5',
+				'3',
+				'1',
+				'2'
+			]
+		}, {
+			name: 'Sam',
+			link: 'https://www.nasa.gov/sites/default/files/styles/image_card_4x3_ratio/public/thumbnails/image/pia20645_main.jpg?itok=dLn7SngD',
+			scores: [
+				'3',
+				'5',
+				'2',
+				'1',
+				'4',
+				'3',
+				'3',
+				'3',
+				'1',
+				'2'
+			]
+		}
+]
